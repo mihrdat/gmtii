fafed930cfbb2523c6919edb25f1bc52c34ffc6d
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-09-02 12:35:15 +0330

    feat: Create Category and Video models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=55)
+    description = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Video(BaseModel):
+    title = models.CharField(max_length=55)
+    description = models.TextField(blank=True, null=True)
+    video = models.FileField(upload_to="blog/videos")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
