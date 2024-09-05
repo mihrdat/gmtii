ce58229448687bd23306412c975f3e1f8699ea20
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-09-05 11:29:06 +0330

    feat: Set permission classes

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,18 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Category, Content
 from .serializers import CategorySerializer, ContentSerializer
 from .pagination import DefaultLimitOffsetPagination
+from .permissions import IsOwnerOrReadOnly, IsAdminOrReadOnly
 
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = DefaultLimitOffsetPagination
+    permission_classes = [IsOwnerOrReadOnly, IsAdminOrReadOnly]
 
 
 class ContentViewSet(ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
     pagination_class = DefaultLimitOffsetPagination
+    permission_classes = [IsOwnerOrReadOnly, IsAdminOrReadOnly]
